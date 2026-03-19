e515075154deee0533cdb1b055c7ff00a376a6f3
Francisco Barberena <francisco.barberena275991@alumnos.info.unlp.edu.ar>
2026-03-19 11:50:19 -0300

    Funcionalidad 1 agregada

--- questions.py
+++ questions.py
@@ -15,30 +15,33 @@ attempts = 6
 print("¡Bienvenido al Ahorcado!")
 print()
 while attempts > 0:
-    # Mostrar progreso: letras adivinadas y guiones para las que faltan
+# Mostrar progreso: letras adivinadas y guiones para las que faltan
     progress = ""
     for letter in word:
         if letter in guessed:
             progress += letter + " "
         else:
             progress += "_ "
-            print(progress)
-            # Verificar si el jugador ya adivinó la palabra completa
-            if "_" not in progress:
-                print("¡Ganaste!")
-                break
-                print(f"Intentos restantes: {attempts}")
-                print(f"Letras usadas: {', '.join(guessed)}")
-                letter = input("Ingresá una letra: ")
-                if letter in guessed:
-                    print("Ya usaste esa letra.")
-                elif letter in word:
-                    guessed.append(letter)
-                    print("¡Bien! Esa letra está en la palabra.")
-                else:
-                    guessed.append(letter)
-                    attempts -= 1
-                    print("Esa letra no está en la palabra.")
-                    print()
-            else:
-                 print(f"¡Perdiste! La palabra era: {word}")
+    print(progress)
+    # Verificar si el jugador ya adivinó la palabra completa
+    if "_" not in progress:
+        print("¡Ganaste!")
+        break
+    print(f"Intentos restantes: {attempts}")
+    print(f"Letras usadas: {', '.join(guessed)}")
+    letter = input("Ingresá una letra: ")
+    if (len(letter)==1 and letter.isalpha()):
+        if letter in guessed:
+            print("Ya usaste esa letra.")
+        elif letter in word:
+            guessed.append(letter)
+            print("¡Bien! Esa letra está en la palabra.")
+        else:
+            guessed.append(letter)
+            attempts -= 1
+            print("Esa letra no está en la palabra.")
+    else:
+        print("Entrada no válida")
+    print()
+else:
+    print(f"¡Perdiste! La palabra era: {word}")
